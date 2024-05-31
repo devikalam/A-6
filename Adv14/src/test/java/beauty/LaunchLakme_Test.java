9cbc8c95f3464ffa750e55c20c5b440767f52756
devis <devis@Devi>
2024-05-31 15:26:05 +0530

    i have added a script "Launch Lakme_Test" inside beauty package

---
+++ Adv14/src/test/java/beauty/LaunchLakme_Test.java
@@ -0,0 +1,12 @@
+package beauty;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class LaunchLakme_Test {
+	@Test
+	public void Lakme() {
+		Reporter.log("lakme application is launching",true);
+	}
+
+}
